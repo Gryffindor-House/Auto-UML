5bc5550ed69365f28abfba513cd053dd6b56cebb
Lalitarvind <lalitarvind.balaji@gmail.com>
2023-12-29 21:23:00 -0700

    changed local host port

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ app = FastAPI()
 
 # Add the dev url of front end
 origins = [
-    "localhost:8000",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+    "localhost:5173",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
     "http://127.0.0.1:5173"
 ]
 
